7231b54a6d6bc6dc58f1e8065d8d43e4a5d5c3c1
cayden <cuiran2001@163.com>
2013-09-17 15:55:53 +0800

    second commit

---
+++ src/com/joke/activity/TestActivity.java
@@ -0,0 +1,26 @@
+/**
+ * TestActivity.java
+ * 版权所有(C) 2013 
+ * 创建:cuiran 2013-9-17 下午3:55:33
+ */
+package com.joke.activity;
+
+import android.app.Activity;
+
+/**
+ * TODO
+ * @author cuiran
+ * @version 1.0.0
+ */
+public class TestActivity extends Activity {
+
+	/**
+	 * TODO
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
